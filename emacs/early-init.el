a64729579fc9a2b9ce57dcaba414f0d959da37f5
Johnny Otsuka <djgoku@icloud.com>
2022-11-23 00:20:16 -0600

    Moved to a three file emacs setup
    The order they are executed are:
    
    1. early-init.el - very minimal setup
    2. init.el - load config.org
    3. config.org - main emacs config that is tangled out to config.el

---
+++ emacs/early-init.el
@@ -0,0 +1,35 @@
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+(straight-use-package 'use-package)
+(straight-use-package 'org)
+
+(use-package modus-themes
+  :straight t
+  :init
+  (setq modus-themes-deuteranopia t
+        modus-themes-org-blocks 'gray-background)
+  (modus-themes-load-themes)
+  :config
+  (modus-themes-load-vivendi))
+
+(use-package undo-fu
+  :straight t
+  :bind
+  (("C-z" . undo-fu-only-undo)
+   ("C-S-z" . undo-fu-only-redo)))
+
+(use-package undo-fu-session
+  :straight t
+  :config
+  (global-undo-fu-session-mode)
+  (setq undo-fu-session-incompatible-files '("/COMMIT_EDITMSG\\'" "/git-rebase-todo\\'")))
