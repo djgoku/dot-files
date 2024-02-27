2c87a7b5ba68e0104e3fe85017a085f12e385be4
Jonathan Carroll Otsuka <pitas.axioms0c@icloud.com>
2024-02-26 20:38:35 -0600

    Updating elpaca which is now using :ensure t vs :elpaca keyword

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -1,7 +1,9 @@
-;; Example Elpaca ealry-init.el -*- lexical-binding: t; -*-
+;; Example Elpaca early-init.el -*- lexical-binding: t; -*-
 
 (setq package-enable-at-startup nil)
 
 ;; Local Variables:
 ;; no-byte-compile: t
+;; no-native-compile: t
+;; no-update-autoloads: t
 ;; End:
