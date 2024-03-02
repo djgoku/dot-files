c27e420d73f20fb2b2b956a4b08c181b530519aa
Jonathan Carroll Otsuka <pitas.axioms0c@icloud.com>
2024-03-01 20:32:02 -0600

    Simplify Earthfile and just use existing .el files

--- earthly/test.el
+++
@@ -1,8 +0,0 @@
-(condition-case e
-    (progn
-      (load "/root/.config/emacs/early-init.el")
-      (load "/root/.config/emacs/init.el")
-      (message "-OK-"))
-  (error
-   (message "ERROR!")
-   (signal (car e) (cdr e))))
