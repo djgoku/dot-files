7d4a67557b712ffbb0da4277297c8580e4ce7daa
Jonathan Carroll Otsuka <105506+djgoku@users.noreply.github.com>
2025-04-08 11:36:47 -0500

    chore: disable `withNativeCompilation` until this is fixed https://github.com/NixOS/nixpkgs/issues/395169

--- emacs-overlay/flake.nix
+++ emacs-overlay/flake.nix
@@ -14,7 +14,10 @@
         };
       in
       rec {
-        packages.default = (pkgs.emacs-git-pgtk.override { }).overrideAttrs (old: rec {
+        packages.default = (pkgs.emacs-git-pgtk.override {
+          # jww (2025-04-02): https://github.com/NixOS/nixpkgs/issues/395169
+          withNativeCompilation = false;
+        }).overrideAttrs (old: rec {
           NIX_CFLAGS_COMPILE = (old.env.NIX_CFLAGS_COMPILE or "")
             + pkgs.lib.optionalString pkgs.stdenv.hostPlatform.isDarwin
             " -DFD_SETSIZE=10000 -DDARWIN_UNLIMITED_SELECT";
